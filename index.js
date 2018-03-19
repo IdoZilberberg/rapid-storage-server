5ab3ede13b660058367c25cfb59c80a38d2b418f
ido <ido@farmdog.ag>
2018-03-19 23:25:28 +0200

    Added /debug route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const filesRouter = require('./api/v1/files-router');
 const path = require('path');
+const os = require('os');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -10,7 +11,16 @@ app
 .use(express.static(path.join(__dirname, 'public')))
 .set('views', path.join(__dirname, 'views'))
 .set('view engine', 'ejs')
+.use('/debug', (req, res) => {
+  res.json({status: 'ok', machine: os.hostname()});
+})
 .use('/', filesRouter)
-
-// .get('/', (req, res) => res.render('pages/index'))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+.use((req, res) => {
+  res.render('404',
+    {locals: {'title': 'Not Found'}},
+    (err, str) => {
+      res.send(str, 404);
+    })
+  // .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+});
